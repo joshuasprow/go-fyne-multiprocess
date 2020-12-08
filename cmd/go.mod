4e29ff9129cf1b3704a1294d73315aeb31d0d6c5
Joshua Sprow <jsprow@gmail.com>
2020-12-08 11:21:24 -0500

    close server button works.

--- cmd/go.mod
+++ cmd/go.mod
@@ -3,7 +3,7 @@ module github.com/joshuasprow/go-fyne-multiprocess/cmd
 go 1.15
 
 require (
-	github.com/joshuasprow/go-fyne-multiprocess/api v0.0.0-20201208151212-e7edc7938d11
+	github.com/joshuasprow/go-fyne-multiprocess/api v0.0.4
 	google.golang.org/genproto v0.0.0-20200806141610-86f49bd18e98 // indirect
 	google.golang.org/grpc v1.34.0
 )
--- cmd/main.go
+++ cmd/main.go
@@ -16,29 +16,54 @@ const (
 // server is used to implement helloworld.GreeterServer.
 type server struct {
 	pb.UnimplementedGreeterServer
+
+	exit chan struct{}
+}
+
+func newServer() *server {
+	return &server{exit: make(chan struct{})}
 }
 
 // SayHello implements helloworld.GreeterServer
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	log.Printf("Received: %v", in.GetName())
+	log.Printf("hello from: %v", in.GetName())
 
 	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayGoodbye(ctx context.Context, in *pb.GoodbyeRequest) (*pb.GoodbyeReply, error) {
+	log.Printf("goodbye from: %v", in.GetName())
+
+	s.exit <- struct{}{}
+
+	return &pb.GoodbyeReply{Message: "Hello " + in.GetName()}, nil
 }
 
 func main() {
 	log.SetPrefix("server: ")
 
-	lis, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
+	srv := newServer()
+	var s *grpc.Server
 
-	s := grpc.NewServer()
+	go func() {
+		lis, err := net.Listen("tcp", port)
+		if err != nil {
+			log.Fatalf("failed to listen: %v", err)
+		}
+		defer lis.Close()
 
-	log.Printf("Server listening on %s", port)
+		s = grpc.NewServer()
 
-	pb.RegisterGreeterServer(s, &server{})
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+		log.Printf("Server listening on %s", port)
+
+		pb.RegisterGreeterServer(s, srv)
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+
+	<-srv.exit
+
+	s.GracefulStop()
 }
